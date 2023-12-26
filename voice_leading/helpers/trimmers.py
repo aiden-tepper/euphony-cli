01f973b9272636396a0b6b92d9144a9e197d5843
Aiden Tepper <aidenjtep@gmail.com>
2023-12-26 15:43:25 -0800

    hidden fifths and octaves

--- voice_leading/helpers/trimmers.py
+++ voice_leading/helpers/trimmers.py
@@ -1,8 +1,22 @@
+def fifth(a, b):
+    for i in range(7, b - a + 1, 12):
+        if b - a % i == 0:
+            return True
+    return False
+
+def octave(a, b):
+    return b - a % 12 == 0
+
+def similar_motion(prev, curr):
+    diff_b = curr[0] - prev[0]
+    diff_s = curr[3] - prev[3]
+    return diff_b > 0 == diff_s > 0
+
 # remove voicings where voices cross each other
 def voice_cross(options):
     return [i for i in options if i[0] < i[1] < i[2] < i[3]]
 
-# remove voicings where S, A, or T are more than an octave apart
+# remove voicings where S, A, or T are more than an 8ve apart
 def octave_apart(options):
     return [i for i in options if i[1] - i[0] <= 12 and i[2] - i[1] <= 12]
 
@@ -16,36 +30,50 @@ def parallel_contrary_fifths(prev, options):
     fifths = []
     for a in range(4):
         for b in range(a+1, 4):
-            interval = prev[b] - prev[a]
-            if interval % 7 == 0:
+            if fifth(prev[a], prev[b]):
                 fifths.append((a, b))
     for curr in options:
         valid = True
         for (a, b) in fifths:
-            interval = curr[b] - curr[a]
-            if interval % 7 == 0:
+            if fifth(prev[a], prev[b]):
                 valid = False
         if valid:
             trimmed.append(curr)
     return trimmed
 
-# remove voicings where any two voicings an octave apart move in parallel (or contrary to another octave)
+# remove voicings where any two voicings an 8ve apart move in parallel (or contrary to another 8ve)
 def parallel_contrary_octaves(prev, options):
     trimmed = []
     octaves = []
     for a in range(4):
         for b in range(a+1, 4):
-            interval = prev[b] - prev[a]
-            if interval % 12 == 0:
+            if octave(prev[a], prev[b]):
                 octaves.append((a, b))
     for curr in options:
         valid = True
         for (a, b) in octaves:
-            interval = curr[b] - curr[a]
-            if interval % 12 == 0:
+            if octave(prev[a], prev[b]):
                 valid = False
         if valid:
             trimmed.append(curr)
+    return trimmed
+
+# remove voicings where S and B move in similar motion to 5th
+def hidden_fifths(prev, options):
+    trimmed = []
+    for curr in options:
+        if similar_motion(prev, curr) and fifth(prev[0], prev[3]):
+            continue
+        trimmed.append(curr)
+    return trimmed
+
+# remove voicings where S and B move in similar motion to 8ve
+def hidden_octaves(prev, options):
+    trimmed = []
+    for curr in options:
+        if similar_motion(prev, curr) and octave(prev[0], prev[3]):
+            continue
+        trimmed.append(curr)
     return trimmed
 
 # if a voicing exists with a common tone, remove non-common tone voicings
@@ -71,6 +99,8 @@ def trim(prev, options):
         options = parallel_contrary_fifths(prev, options)
         options = parallel_contrary_octaves(prev, options)
         options = common_tone(prev, options)
+        options = hidden_fifths(prev, options)
+        options = hidden_octaves(prev, options)
     return options
 
 if __name__ == "__main__":
